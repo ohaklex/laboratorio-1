3baa0b3591eb2af8684e2ef5d55a0c82fd2d96c1
ohaklex <00254125@uca.edu.sv>
2025-04-28 08:37:40 -0600

    Add files via upload

---
+++ trabajo 4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int precioParque, edad;
+    cout << "ingrese la edad";
+    cin >> edad;
+
+    if (edad < 5)
+    {
+        cout << "gratis";
+    }
+    else if (edad > 5)
+    {
+        cout << "pagan 5";
+    }
+    else if (edad > 12)
+    {
+
+        cout << "pagan 10";
+    }
+    else if (edad > 12)
+    {
+        cout << "pagan 10";
+    }
+    else
+    {
+        cout << "menore de 10 pagan 5"
+    }
+}
